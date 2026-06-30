acc2af232f9951164da5632425a6180b64a524e6
Haseeb <haseebur003@gmail.com>
2026-06-30 23:51:18 +0500

    Updated Login & Signup

--- src/app/auth/Signup/page.js
+++ src/app/auth/Signup/page.js
@@ -2,15 +2,14 @@ import React from 'react'
 
 import Overview from '../../../components/overview'
 import { Login } from '../../../components/Login'
-import { Signup } from '../../../components/Signup'
+import SignupPage, { Signup } from '../../../components/Signup'
 
 
 function Page() {
   return (
     <div className='bg-gray-100 bg-linear-to-br from-blue-200 via-white to-purple-200   min-h-screen backdrop-blur-4xl'>
         <div className=''>
-            <Signup/>
-            {/* <Login/> */}
+            <SignupPage/>
         </div>
     </div>
   )
